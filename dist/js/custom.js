fe5dd0d19892e45208efe751bb04c051a8887319
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2024-08-07 17:26:19 +0900

    틀작업 진행중

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -90,12 +90,53 @@ const headerScript = ()=>{
 
 }
 
-// 헤더 스크립트
+// 사이드메뉴 스크립트
 const sideMenuScript = ()=>{
+    $(".side-menu").on("click", function () {
+        if ($(this).parent().find("ul").length) {
+            if ($(this).parent().find("ul").first()[0].offsetParent !== null) {
+                $(this)
+                    .find(".side-menu__sub-icon")
+                    .removeClass("transform rotate-180");
+                $(this).removeClass("side-menu--open");
+                $(this)
+                    .parent()
+                    .find("ul")
+                    .first()
+                    .slideUp(300, function () {
+                        $(this).removeClass("side-menu__sub-open");
+                    });
+            } else {
+                $(this)
+                    .find(".side-menu__sub-icon")
+                    .addClass("transform rotate-180");
+                $(this).addClass("side-menu--open");
+                $(this)
+                    .parent()
+                    .find("ul")
+                    .first()
+                    .slideDown(300, function () {
+                        $(this).addClass("side-menu__sub-open");
+                    });
+            }
+        }
+    });
 
 }
 
 // 모달 스크립트
 const modalScript = ()=>{
 
+}
+
+// 로딩 스크립트
+window.addEventListener('load',function(){
+    setTimeout(function(){
+        $('#initial-loader').addClass('close')
+    },1000)
+})
+
+const sideToggle = ()=>{
+    const wrap = document.querySelector("#wrap");
+    wrap.classList.toggle("fold");
 }
